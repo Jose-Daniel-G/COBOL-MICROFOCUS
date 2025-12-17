1df32ae8d96b5dbb2c63d8128d342dfa1f6bf291
Jose Daniel <jose.grijalba04@gmail.com>
2025-12-17 07:43:32 -0500

    blink reversed <= it had been delete

--- example4.cbl
+++ example4.cbl
@@ -12,10 +12,10 @@
            DECIMAL-POINT IS COMMA.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
-            COPY "./sel/clientes.sel"
+            COPY "./clientes.sel".
        DATA DIVISION.
        FILE SECTION.
-            COPY "./fd/clientes.fd"
+            COPY "./clientes.fd".
 
        WORKING-STORAGE SECTION.
        01  ST-FILE    PIC XX.
@@ -23,21 +23,21 @@
 
        01  MENSAJE    PIC X(70).
        01  FIN        PIC X     VALUES "N".
-       01  GUIONES    PIC X(80) VALUES H"C4".
+       01  GUIONES    PIC X(80) VALUES ALL "-".
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
            PERFORM INICIALIZACION.
            PERFORM ABRO-ARCHIVO.
            PERFORM PROCESO THRU F-PROCESO UNTIL FIN = "S".
-           PERFORM CIERRO-ARCHIVO.
+           PERFORM CIERRE-ARCHIVO.
            GO TO FINALIZAR.
 
        INICIALIZACION.
            MOVE "N" TO FIN.
        ABRO-ARCHIVO.
            OPEN I-O CLIENTES.
-           IF ST-SIC > "07"
-              STRING "Error al abrir Clientes " ST-SIC DELIMITED BY SIZE
+           IF ST-FILE > "07"
+             STRING "Error al abrir Clientes " ST-FILE DELIMITED BY SIZE
                      INTO MENSAJE
               DISPLAY MENSAJE LINE 10 COL 20
               MOVE "S" TO FIN.
@@ -53,14 +53,15 @@
            EXIT.
 
        MUESTRO-PANTALLA.
-           DISPLAY " "                 LINE 1  COL 1 ERASE EOS
+           DISPLAY " "                 LINE 1  COL 1
                    "A.B.M Clientes"    LINE 3  COL 32
                    GUIONES             LINE 4  COL 1
-                   "Id Cliente     : " LINE 10 COL 10
-                   "01 Nombre      : " LINE 12 COL 10
-                   "02 Direccion   : " LINE 14 COL 10
-                   "03 Cod. Postal : " LINE 16 COL 10
-                   "04 Categoria   : " LINE 18 COL 10
-                   "Opcion []      : " LINE 18 COL 10
-                   GUIONES          LINE 22  COL 1.
+                   "Id Cliente     : " LINE 07 COL 10
+                   "01 Nombre      : " LINE 10 COL 10
+                   "02 Direccion   : " LINE 12 COL 10
+                   "03 Cod. Postal : " LINE 14 COL 10
+                   "04 Categoria   : " LINE 16 COL 10
+                   "05 Opcion []      : " LINE 20 COL 30
+                   GUIONES             LINE 22  COL 1.
+           DISPLAY " " LINE 20 COL 40.
        END PROGRAM "JD-TWINS".
